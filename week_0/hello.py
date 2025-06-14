8f4cfe70581d0e91672256893e1e533c292e4d90
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-14 17:45:20 +0530

    variables, operators'

--- week_0/hello.py
+++ week_0/hello.py
@@ -2,3 +2,12 @@
 # print is a basic function to print something on the terminal
 # arguments: anything written in between the parenthesis '()' symbol is an arg
 print("Hello, World!")
+
+# Input Function: takes in input from user
+# syntax: input()
+# input("What's your name? ")
+
+# Variables
+# syntax: <varname>
+# Assignment operator '=', value from the right of equal to symbol is assigned to left
+name = input("What's your name?")
