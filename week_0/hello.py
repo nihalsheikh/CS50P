d02c413991b7be8a5aaee68a9f7df197f1ac290d
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-14 18:40:06 +0530

    end and sep params

--- week_0/hello.py
+++ week_0/hello.py
@@ -5,9 +5,19 @@ print("Hello, World!")
 
 # Input Function: takes in input from user
 # syntax: input()
-# input("What's your name? ")
+input("Provide an input: ")
 
 # Variables
 # syntax: <varname>
 # Assignment operator '=', value from the right of equal to symbol is assigned to left
-name = input("What's your name?")
+name = input("Enter your name?")
+
+# separating args with comma
+print("hello,", name)
+
+# how to print in a single line, if we use 2 print func
+# we can pass additional params such as `end` and `sep`
+# end="\n" takes you to next line
+# sep=" " adds space between 2 args
+print("hello,", end="")
+print(name)
