a0c63cb7735d729edebf9ac53e0e2d56400286f3
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 11:22:31 +0530

    Scope

--- week_0/function.py
+++ week_0/function.py
@@ -1,14 +1,22 @@
 # Function in python
 # def: this keyword is used to create or define a function
-def hello(): # no args and default values specified here
-    print("hello")
+# def hello(): # no args and default values specified here
+    # print("hello")
 
 # hello() # fucn with no args and default values
 
-def greet(toWho="world"):
-    print("hello,", toWho)
+# def greet(toWho="world"):
+#     print("hello,", toWho)
 
-greet() # func with default value only
+# greet() # func with default value only
 
-name = input("Enter name: ")
-greet(name) # func with an argument
+# name = input("Enter name: ")
+# greet(name) # func with an argument
+
+# Scope: context of a vairable's existence in which it is defined
+def main():
+    name = input("Enter name: ") # name var is defined in main function
+    print("hello,", name)
+
+def greet2():
+    print(name) # name is defined in main func and we can't access it from another func directly
