50e95ca950927ed62310093950267af2a0a8201e
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 14:52:06 +0530

    folder update

--- problem_set_0/indoor/indoor.py
+++
@@ -1,4 +0,0 @@
-# Problem set 0
-# code to make all typed string in lowercase
-voice = str(input("Say something: ")).lower()
-print(voice)
--- problem_set_0/playback/playback.py
+++
@@ -1,5 +0,0 @@
-# add 3 dots instead of spaces in between strings
-# lets take input of the speaker
-# we use replace method to replace space with dots
-speak = str(input("Speak: ")).replace(" ", '...')
-print(speak)
