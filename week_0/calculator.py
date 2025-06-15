1d2940cf7f141e759f378c2c804452478b359283
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 08:46:40 +0530

    Nesting and Nested Function

--- week_0/calculator.py
+++ week_0/calculator.py
@@ -4,13 +4,22 @@
 # y = 2
 
 # 2. Taking input from user
-x = input("Enter x: ")
-y = input("Enter y: ")
+# x = input("Enter x: ")
+# y = input("Enter y: ")
 
 # There is a problem in directly using the values of x & y var in z
 # python will treat them as a str values rather than int
 # z = x + y
 
 # to get rid of this issue we need to use int func 'int()' as follows
-z = int(x) + int(y)
-print(z)
+# z = int(x) + int(y)
+# print(z)
+
+# Nesting functions: function within a function is called nesting
+# ex: we used input func within the int func
+# the args taken by input func becomes the param value passed to int func
+x = int(input("Enter x: " ))
+y = int(input("Enter y: " ))
+
+# now we can directly use x & y var & instead of using them in another var
+print(x + y)
