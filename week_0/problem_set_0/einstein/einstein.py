587f3c1ac0399b22f55519a6dcd14f6c9c9d13b5
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 15:36:30 +0530

    Week 0: Problem 4 completed

--- week_0/problem_set_0/einstein/einstein.py
+++ week_0/problem_set_0/einstein/einstein.py
@@ -1,1 +1,11 @@
 # Week 0: Problem 4
+# calculate E=mc**2 formula
+def formula(mass):
+    E = mass * (300000000)**2
+    return E
+
+def main():
+    mass = int(input("Enter mass in kg: "))
+    print(formula(mass))
+
+main()
