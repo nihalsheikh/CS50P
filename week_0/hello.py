ac8c30e0ec6d76132f1148b496d57da7e0f71e4c
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 07:46:43 +0530

    Method Chaining

--- week_0/hello.py
+++ week_0/hello.py
@@ -1,36 +1,43 @@
 # Functions
 # print is a basic function to print something on the terminal
 # arguments: anything written in between the parenthesis '()' symbol is an arg
-print("Hello, World!")
+# print("Hello, World!")
 
 # Input Function: takes in input from user
 # syntax: input()
-input("Provide an input: ")
+# input("Provide an input: ")
 
 # Variables
 # syntax: <varname>
 # Assignment operator '=', value from the right of equal to symbol is assigned to left
-name = input("Enter your name?")
+# name = input("Enter your name?")
 
 # separating args with comma
-print("hello,", name)
+# print("hello,", name)
 
 # how to print in a single line, if we use 2 print func
 # we can pass additional params such as `end` and `sep`
 # end="\n" takes you to next line
 # sep=" " adds space between 2 args
-print("hello, ", end="")
-print(name)
+# print("hello, ", end="")
+# print(name)
 
 # String Methods
 # 1. strip() - this method removes whitespaces from left and right of a string
 # ex: .....john.... => john
-name = name.string()
+# name = name.string()
 
 # 2. capitalize() - this methods makes the first letter of the word of a string capital
 # ex: john doe => John doe
-name = name.capitalize()
+# name = name.capitalize()
 
 # 3. title() - this method will capitalize the first letter of every word in a string
 # ex: john doe => John Doe
-name = name.title()
+# name = name.title()
+
+# Method Chaining
+# we can write multiple methods in a single line using (.) dot symbol, instead of writing in multiple lines
+name = input("Enter name: ").strip().title()
+
+# another special dynamic string writing syntax
+print(f"hello, {name}")
