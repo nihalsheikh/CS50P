19c3ffe81cf6e00e301067b21b9acabf98785a52
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-15 11:04:39 +0530

    function with args and default value

--- week_0/function.py
+++ week_0/function.py
@@ -1,6 +1,14 @@
 # Function in python
 # def: this keyword is used to create or define a function
-def hello():
+def hello(): # no args and default values specified here
     print("hello")
 
-hello()
+# hello() # fucn with no args and default values
+
+def greet(toWho="world"):
+    print(f"hello, {toWho}")
+
+greet() # func with default value only
+
+name = input("Enter name: ")
+greet(name) # func with an argument
