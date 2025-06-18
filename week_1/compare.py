b61ddfeefc6d7709af9e4b3b9ec9801544932227
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-18 21:50:03 +0530

    if, elif, else & or keywords

--- week_1/compare.py
+++ week_1/compare.py
@@ -13,9 +13,26 @@ y = int(input("Enter y: "))
 
 # elif
 # when you have multiple questions istead of using only 'if' keyword we use 'elif' to be more inclusive
-if x < y :
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
-elif x == y:
+# if x < y :
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x is equal to y")
+
+
+# else
+# used at last, when we know the all the other values of bool exp will return false
+# if x < y :
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else: # no need to write bool exp for 'else'
+#     print("x is equal to y") # executed only when other (above) conditions for bool exp return false
+
+# or
+# used to reduce the no. of questions asked
+if x < y or x > y:
+    print("x is not equal to y")
+else:
     print("x is equal to y")
