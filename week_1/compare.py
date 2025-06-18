a21626cf50c4976b818200d197ec4e4fa30d59b1
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-18 21:11:29 +0530

    Conditionals started

---
+++ week_1/compare.py
@@ -0,0 +1,21 @@
+# Compare values
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+# if keyword
+# syntax: if > Boolean expression > : ><nextLine> some code/func
+# if x < y :
+#     print("x is less than y")
+# if x > y:
+#     print("x is greater than y")
+# if x == y:
+#     print("x is equal to y")
+
+# elif
+# when you have multiple questions istead of using only 'if' keyword we use 'elif' to be more inclusive
+if x < y :
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
