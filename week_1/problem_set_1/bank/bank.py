45f2cd83a06797b7b8098d978a39ffa1b5e87098
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-19 22:03:30 +0530

    Week 1: Problem 1 solved

--- week_1/problem_set_1/bank/bank.py
+++ week_1/problem_set_1/bank/bank.py
@@ -1,16 +1,16 @@
 # greet hello
 def main():
-    greet = input("Greetings: ")
-    greet = greet.lower().strip()
-    g, n = greet.split()
-    greetMe(g)
+    greet = input("Greetings: ") # take input from user
+    greet = greet.lower().strip() # make sure input is in lower case and no white spaces
+    greetMe(greet) # calling greetMe func with user input
 
 def greetMe(g):
-    if(g == "hello"):
+    # 'startswith' method: use this method, when searching for a specific string in the start
+    if g.startswith("hello"):
         print("$0")
-    elif(g != "hello"):
-        g = g.lstrip()
-        if g == "h":
-            print("$20")
+    elif g.startswith("h"):
+        print("$20")
     else:
         print("$100")
+
+main()
