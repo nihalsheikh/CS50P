a1070622a535df7af37c9d0bf9e3632bfccad62a
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-19 16:30:30 +0530

    match and _ keywords in use

---
+++ week_1/house.py
@@ -0,0 +1,40 @@
+# match keyword
+# this is a Switch case in Python
+# for default case we use underscore symbol (_)
+
+name = input("What's your name? ")
+
+# if name == "Harry":
+#     print("Gryffindor")
+# elif name == "Hermione":
+#     print("Gryffindor")
+# elif name == "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+# the above code is not a good practise
+# so we use the switch or match case here in python
+# match name:
+#     case "Harry":
+#         print("Gryffindor")
+#     case "Hermione":
+#         print("Gryffindor")
+#     case "Ron":
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin")
+#     case _:
+#         print("Who?")
+
+# the above match is also repititive
+# we can refine it more, like follows
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who? ")
