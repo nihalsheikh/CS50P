d546058558908e8bd381a635913ac2b861e937dd
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-19 15:32:30 +0530

    uodated if conditional grade code

--- week_1/grade.py
+++ week_1/grade.py
@@ -26,13 +26,24 @@ score = int(input("Enter Score: "))
 
 # We can again improve the above code and completely remove the 'and' keyword usage in code
 # Python allows the following kind of nesting and chainng of bool exp in conditional statements
-if 90 <= score <= 100:
+# if 90 <= score <= 100:
+#     print("Grade A")
+# elif 80 <= score < 90:
+#     print("Grade B")
+# elif 70 <= score < 80:
+#     print("Grade C")
+# elif 60 <= score < 70:
+#     print("Grade D")
+# else:
+#     print("Grade F")
+
+if score >= 90:
     print("Grade A")
-elif 80 <= score < 90:
+elif score >= 80:
     print("Grade B")
-elif 70 <= score < 80:
+elif score >= 70:
     print("Grade C")
-elif 60 <= score < 70:
+elif score >= 60:
     print("Grade D")
 else:
     print("Grade F")
