446dfa803f9d4a8e85c3ac56aa7ca71750a939af
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-30 11:10:12 +0530

    Problem 4 submitted

--- week_1/problem_set_1/interpreter/interpreter.py
+++ week_1/problem_set_1/interpreter/interpreter.py
@@ -1,10 +1,26 @@
 # Week 1: Problem 4
+# Solve arithmetic expressions provided by user
 def main():
-    e = input("Enter Expression: ")
-    solve(e)
+    e = input("Enter an expression: ").strip()
+    print(perform(e))
 
-def solve(e):
-    a,b,c = e.split(" ")
-    print(a, c, b)
+def perform(exp):
+    a,b,c = exp.split(" ")
+    a = float(a)
+    c = float(c)
 
+    match b:
+        case "+":
+            return float(a + c)
+        case "-":
+            return float(a - c)
+        case "*":
+            return float(a * c)
+        case "/":
+            return float(a / c)
+        case "%":
+            return float(a % c)
+        case _:
+            return "In"
+        
 main()
