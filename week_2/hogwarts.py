5dd0b13e2a85aad0f703ab31788eb56022240973
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-02 13:24:31 +0530

    using list with loops

---
+++ week_2/hogwarts.py
@@ -0,0 +1,10 @@
+# lists: lists are zero-indexed, meaning their posisiton counting starts from 0
+students = ["Hermione", "Harry", "Ron"]
+
+# printing an list item with its index
+#  to access a list item, write list name and its index inside the square bracket
+# print(students[0])
+
+# we can print the list items using loops, isntead of printing them manually by assigning index individually
+for student in students:
+    print(student)
