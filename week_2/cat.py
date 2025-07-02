b6b75fd08864bce79dc9dcbfda14ff89e358a276
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-02 09:27:47 +0530

    Week 2: Loops Started

---
+++ week_2/cat.py
@@ -0,0 +1,20 @@
+# Week 2: Loops
+
+# While Loop
+# Print meow 3 times:
+# print("meow")
+# print("meow")
+# print("meow")
+
+# using while loop to execute the same code as above
+print("While Loop:")
+i = 3
+while i != 0:
+    print("meow")
+    i -= 1 # earlier we wrote i = i - 1
+
+# same code as above, just different logic
+i = 0
+while i < 3:
+    print("meow")
+    i += 1 # earlier we wrote i = i + 1
