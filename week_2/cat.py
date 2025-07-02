f9fbce309fa93137f61e7cc9c0e5a162c821e031
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-02 09:47:01 +0530

    while and for loop

--- week_2/cat.py
+++ week_2/cat.py
@@ -1,20 +1,27 @@
 # Week 2: Loops
 
-# While Loop
 # Print meow 3 times:
 # print("meow")
 # print("meow")
 # print("meow")
 
+# While Loop
 # using while loop to execute the same code as above
-print("While Loop:")
-i = 3
-while i != 0:
-    print("meow")
-    i -= 1 # earlier we wrote i = i - 1
+# print("While Loop")
+# i = 3
+# while i != 0:
+#     print("meow")
+#     i -= 1 # earlier we wrote i = i - 1
 
-# same code as above, just different logic
-i = 0
-while i < 3:
+# # same code as above, just different logic
+# i = 0
+# while i < 3:
+#     print("meow")
+#     i += 1 # earlier we wrote i = i + 1
+
+# For loop
+# using for loop to perform same tasks as above
+# List: a datatype in python, to make a list use square brackets `[]` add items by separation using comma `,`
+print("For Loop")
+for i in [0,1,2]: # list is [0,1,2]
     print("meow")
-    i += 1 # earlier we wrote i = i + 1
