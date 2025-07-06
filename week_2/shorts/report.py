b27e5174140c1c40e02228040d297b77671b4aca
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-06 08:40:57 +0530

    Dictionary in depth

---
+++ week_2/shorts/report.py
@@ -0,0 +1,20 @@
+# Report statistics of Spaceship
+def main():
+    spacecraft = {"name": "Voyager 1", "distance": "163"}
+
+    print(create_report(spacecraft))
+
+def create_report(spacecraft):
+    return f"""
+
+    ===== SpaceCraft Details =====
+
+     Name: {spacecraft["name"]}
+
+     Distance: Distance in AU
+
+    ======== Report Ends =========
+
+    """
+
+main()
