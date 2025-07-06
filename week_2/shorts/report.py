81a43e5a2521f6454a266ed362fe8da0f95cfac6
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-06 09:14:25 +0530

    update method in dictionary

--- week_2/shorts/report.py
+++ week_2/shorts/report.py
@@ -1,27 +1,17 @@
 # Report statistics of Spaceship
 def main():
-    # print(create_report(spacecraft)) # used for Method 1 & 2
-    distances = {
-        "Voyager 1": 163,
-        "Voyager 2": 136,
-        "Pioneer 10": 80,
-        "Pioneer 11": 44,
-        "New Horizon": 58,
-    }
+    spacecraft = {"name": "Voyager 1"}
+    spacecraft.update(
+        {
+            "distance": 163,
+            "orbit": "Sun",
+            "planet": "Venus"
+        }
+    )
 
-    # Key method: For looping over the key name in a dictionary
-    # for name in distances.keys():
-    #     print(f"{name} is {distances[name]} AU away from earth")
+    print(create_report(spacecraft))
 
-    # Value method: for looping over the values in the dictionary
-    for distance in distances.values():
-        print(f"{distance} AU is {convert(distance)} m")
-
-def convert(au):
-    # Converting AU distance in meters
-    return au * 149597870700
-
-def create_report(spacecraft): # used for Method 1 & 2 only
+def create_report(spacecraft):
     # Method 1: Normal access to dictionaries
     # return f"""
     # ===== SpaceCraft Details =====
