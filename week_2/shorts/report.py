f3e7a879d814a2a37ec5b4398a765ad99b78972d
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-06 08:47:00 +0530

    Explicit key define and get method

--- week_2/shorts/report.py
+++ week_2/shorts/report.py
@@ -1,20 +1,29 @@
 # Report statistics of Spaceship
 def main():
-    spacecraft = {"name": "Voyager 1", "distance": "163"}
+    spacecraft = {"name": "Voyager 1"}
+    spacecraft["distance"] = 163
 
     print(create_report(spacecraft))
 
 def create_report(spacecraft):
-    return f"""
+    # Method 1: Normal access to dictionaries
+    # return f"""
+    # ===== SpaceCraft Details =====
+    #  Name: {spacecraft["name"]}
+    #  Distance: {spacecraft["distance"]} AU
+    # ======== Report Ends =========
+    # """
 
+    # Method 2: Using get() method to avoid error
+    return f"""
     ===== SpaceCraft Details =====
 
-     Name: {spacecraft["name"]}
+     Name: {spacecraft.get("name", "Unknown")}
 
-     Distance: {spacecraft["distance"]} AU
+     Distance: {spacecraft.get("distance", "Unknown")} AU
 
     ======== Report Ends =========
-
     """
+
 
 main()
