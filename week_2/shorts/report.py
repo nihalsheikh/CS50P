b86d730d9ec80902fd5fecb50ab4ac1d0bbd8c13
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-06 09:00:11 +0530

    keys method in dictionaries

--- week_2/shorts/report.py
+++ week_2/shorts/report.py
@@ -1,17 +1,19 @@
 # Report statistics of Spaceship
 def main():
-    spacecraft = {"name": "Voyager 1"}
-    spacecraft.update( # Adding multiple key-values at once to dictionary
-        {
-            "distance": 163,
-            "orbit": "Sun",
-            "planet": "Venus"
-        }
-    )
+    # print(create_report(spacecraft)) # used for Method 1 & 2
+    spacecraft = {
+        "Voyager 1": 163,
+        "Voyager 2": 136,
+        "Pioneer 10": 80,
+        "Pioneer 10": 44,
+        "New Horizon": 58,
+    }
 
-    print(create_report(spacecraft))
+    for name in spacecraft.keys():
+        print(f"{name} is {spacecraft[name]} AU away from earth")
 
-def create_report(spacecraft):
+
+def create_report(spacecraft): # used for Method 1 & 2 only
     # Method 1: Normal access to dictionaries
     # return f"""
     # ===== SpaceCraft Details =====
@@ -21,6 +23,16 @@ def create_report(spacecraft):
     # """
 
     # Method 2: Using get() method to avoid error
+    # return f"""
+    # ===== SpaceCraft Details =====
+    #  Name: {spacecraft.get("name", "Unknown")}
+    #  Distance: {spacecraft.get("distance", "Unknown")} AU from Earth
+    #  Orbit: in {spacecraft.get("orbit", "Unknown")} Orbit
+    #  Planet: going towrads {spacecraft.get("planet", "Unknown")}
+    # ======== Report Ends =========
+    # """
+
+    # Method 3: Looping on dictionary using key method to get key name
     return f"""
     ===== SpaceCraft Details =====
 
