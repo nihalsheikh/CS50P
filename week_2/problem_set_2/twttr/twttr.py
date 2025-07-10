252bbb7dcbfff7bd16125d5bf19b218e07838c81
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-10 19:51:06 +0530

    Problem 3 solved

--- week_2/problem_set_2/twttr/twttr.py
+++ week_2/problem_set_2/twttr/twttr.py
@@ -1,1 +1,23 @@
 # Week 2: Problem 3
+# User inputs a string, remove vowels from it to shorten it
+def main():
+    # User input and remove the whitespaces from left and right
+    text = input("Enter Text: ").strip()
+    print(shorten(text))
+
+def shorten(user_text):
+    # new word without vowels initialized
+    short_word = ""
+
+    # Tuple of vowels
+    vowels = ("a", "e", "i", "o" , "u", "A", "E", "I", "O", "U")
+
+    # Method 1: Through loops and Conditions
+    # go through every char in user_text and check if exist in vowels
+    for c in user_text:
+        if c not in vowels:
+            short_word += c
+
+    return f"Short Word: {short_word}"
+
+main()
