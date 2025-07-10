a8613b339b7d7c95db7f095aa9c1d4d437ebc1c2
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-10 11:43:17 +0530

    Problem 2 solved and submitted

--- week_2/problem_set_2/camel/camel.py
+++ week_2/problem_set_2/camel/camel.py
@@ -8,12 +8,26 @@ def main():
 def variable_name():
     # Repeating the input prompt till the str length is > 0
     while True:
-        name = input("Enter camelCase: ").strip()
+        name = input("camelCase: ").strip()
         if len(name) >  0:
             return name
 
 def snake_case(var_name):
-    if len(var_name) > 0:
-        return 2
+       
+    new_word = ""
+    # Check every char in the string through loop
+    for char in var_name:
+        # when the char in string is Uppercase
+        if char.isupper():
+            # check whether the char is at the start of the string
+            if new_word != "":
+                new_word = new_word + "_" # add _ only when Uppercase char is not at the start
+
+            new_word = new_word + char.lower() # and then add the lowercase version of that char
+        else:
+            # all the other the time when char is not uppercase, keep it adding to our new_word
+            new_word = new_word + char
+
+    return f"snake_case: {new_word}"
 
 main()
