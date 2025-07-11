2c04c3fc4c09544c2876e683e18d93b2e19330db
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-11 17:17:47 +0530

    Problem 5 cleaned & optimized

--- week_2/problem_set_2/nutrition/nutrition.py
+++ week_2/problem_set_2/nutrition/nutrition.py
@@ -5,34 +5,6 @@ def main():
     print(calc_calorie(item))
 
 def calc_calorie(item):
-    # Method 1: List inside Dictionary
-    # fruits = [
-    #     {"fruit" : "Apple", "calories": 130},
-    #     {"fruit" : "Avocado", "calories": 50},
-    #     {"fruit" : "Banana", "calories": 110},
-    #     {"fruit" : "Cantaloupe", "calories": 50},
-    #     {"fruit" : "Grapefruit", "calories": 60},
-    #     {"fruit" : "Grape", "calories": 90},
-    #     {"fruit" : "Honeydew Melon", "calories": 50},
-    #     {"fruit" : "Kiwifruit", "calories": 90},
-    #     {"fruit" : "Lemon", "calories": 15},
-    #     {"fruit" : "Lime", "calories": 20},
-    #     {"fruit" : "Nectarine", "calories": 60},
-    #     {"fruit" : "Orange", "calories": 80},
-    #     {"fruit" : "Peach", "calories": 60},
-    #     {"fruit" : "Pear", "calories": 100},
-    #     {"fruit" : "Pineapple", "calories": 50},
-    #     {"fruit" : "Plums", "calories": 70},
-    #     {"fruit" : "Strawberries", "calories": 50},
-    #     {"fruit" : "Sweet Cherries", "calories": 100},
-    #     {"fruit" : "Tangerine", "calories": 50},
-    #     {"fruit" : "Watermelon", "calories": 80}
-    # ]
-
-    # for fru in fruits:
-    #     if item == fru["fruit"].lower():
-    #         return f"Calories: {fru["calories"]}"
-
     # Method 2: Dictionary with .get method
     fruits = {
         "apple": 130,
@@ -56,8 +28,6 @@ def calc_calorie(item):
         "tangerine": 50,
         "watermelon": 80
     }
-
-    # fruit = item.lower()
 
     if item in fruits:
         return f"Calories: {fruits[item]}"
