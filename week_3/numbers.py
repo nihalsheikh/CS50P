a79989c1855eb30a1f491a939d749a6958e73429
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-12 09:03:34 +0530

    else with try and except

--- week_3/numbers.py
+++ week_3/numbers.py
@@ -1,8 +1,9 @@
 # Week 3: Exceptions
 # try and except
-
+# else
 try:
     x = int(input("What's x? "))
-    print(f"x is {x}")
 except ValueError:
     print("x is not an integer")
+else:
+    print(f"x is {x}")
