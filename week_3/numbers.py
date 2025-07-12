56387c19c1471731dbb2c177447a3fd3aeb60141
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-12 10:40:56 +0530

    Cleaned code

--- week_3/numbers.py
+++ week_3/numbers.py
@@ -1,16 +1,17 @@
 # Week 3: Exceptions
 # try and except
-
+# Added params
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
 # else
 # pass
-def get_int():
+# pass an arg
+def get_int(num):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(num))
         except ValueError:
             pass # catching the error, but not doing anything about/on it
 
