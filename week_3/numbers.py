be617065a5f391379e155538fcb57219c4024419
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-12 10:29:52 +0530

    defined functions

--- week_3/numbers.py
+++ week_3/numbers.py
@@ -1,12 +1,15 @@
 # Week 3: Exceptions
 # try and except
 # else
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+
+main()
