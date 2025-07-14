cc3b216d4bdadf35bdaed10d11e6bf5620f9b905
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-14 22:43:40 +0530

    Problem 2 started

---
+++ week_3/problem_set_3/taqueria/taqueria.py
@@ -0,0 +1,30 @@
+# Week 3: Problem 2
+# Order from the Felipe Taqueria restaurant
+def main():
+    menu = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    total = 0.00
+
+    while True:
+        try:
+            item = input("Enter dish: ").strip()
+            if item in menu:
+                total += menu[item]
+                print(f"Total: ${total}")
+        except (KeyError, ValueError, TypeError):
+            pass
+        except EOFError:
+            print()
+            break
+
+main()
