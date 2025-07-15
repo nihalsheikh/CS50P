b959ce529de92d1d9085c6ff84ab6ea2822d5efa
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-15 07:27:33 +0530

    Problem 2 submitted

--- week_3/problem_set_3/taqueria/taqueria.py
+++ week_3/problem_set_3/taqueria/taqueria.py
@@ -1,6 +1,8 @@
 # Week 3: Problem 2
 # Order from the Felipe Taqueria restaurant
 def main():
+    # Dictionary of menu items and prices
+    # writing `key` in lowercase, because we are making program case-insensitive
     menu = {
         "baja taco": 4.25,
         "burrito": 7.50,
@@ -13,18 +15,23 @@ def main():
         "tortilla salad": 8.00
     }
 
+    # Initializing total price
     total = 0.00
 
+    # To ask user for infinite inputs
     while True:
         try:
+            # Input is case-insenitive and remove whitespaces
             item = input("Item: ").strip().lower()
             if item in menu:
+                # Adding the value of each item input to the total price
                 total += menu[item]
                 print(f"Total: ${total:.2f}")
+        # Catching all the errors and doing something about it.
         except (KeyError, ValueError, TypeError):
             pass
         except EOFError:
-            print()
+            print() # going to next line
             break
 
 main()
