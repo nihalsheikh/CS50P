ccf2b92bd74a36bab2cb8ce07af886fa0c808ad2
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-07-15 07:06:12 +0530

    Problem 2 solved adn submitted

--- week_3/problem_set_3/taqueria/taqueria.py
+++ week_3/problem_set_3/taqueria/taqueria.py
@@ -2,25 +2,25 @@
 # Order from the Felipe Taqueria restaurant
 def main():
     menu = {
-        "Baja Taco": 4.25,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00
+        "baja taco": 4.25,
+        "burrito": 7.50,
+        "bowl": 8.50,
+        "nachos": 11.00,
+        "quesadilla": 8.50,
+        "super burrito": 8.50,
+        "super quesadilla": 9.50,
+        "taco": 3.00,
+        "tortilla salad": 8.00
     }
 
     total = 0.00
 
     while True:
         try:
-            item = input("Enter dish: ").strip()
+            item = input("Item: ").strip().lower()
             if item in menu:
                 total += menu[item]
-                print(f"Total: ${total}")
+                print(f"Total: ${total:.2f}")
         except (KeyError, ValueError, TypeError):
             pass
         except EOFError:
