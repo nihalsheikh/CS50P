eea71d68c088d5ae1163675ab5d8992b4ba5cbe0
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-16 12:29:03 +0530

    Using my own library

--- week_4/say.py
+++ week_4/say.py
@@ -1,8 +1,17 @@
 # Week 4: Libraries
 # Using Packages
-import cowsay
-import sys
+# import cowsay
+# import sys
 
+# if len(sys.argv) == 2:
+#     # cowsay.cow("hello, " + sys.argv[1])
+#     cowsay.trex("hello, " + sys.argv[1])
+
+
+# Using my own library
+import sys
+from sayings import hello
+
+# only call the function when arg provided
 if len(sys.argv) == 2:
-    # cowsay.cow("hello, " + sys.argv[1])
-    cowsay.trex("hello, " + sys.argv[1])
+    hello(sys.argv[1])
