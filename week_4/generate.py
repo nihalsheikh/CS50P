ab658fbf5a268a93333f1b4720aa14db4259f2ac
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-16 07:54:34 +0530

    shuffle function

--- week_4/generate.py
+++ week_4/generate.py
@@ -13,7 +13,7 @@ import random
 
 # shuffle the list items
 # list of type of cards
-cards = ["diamondaaa", "heart", "spade", "club"]
+cards = ["diamond", "heart", "spade", "club"]
 # shuffle function in random module
 random.shuffle(cards)
 # print card individually
