c4ba5a3a316b89d01d8e0fc9a62377ed17cf5269
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-16 07:23:33 +0530

    randint(a, b) function

--- week_4/generate.py
+++ week_4/generate.py
@@ -1,8 +1,12 @@
 # Week 4: Libraries
 # import keyword
 # from keyword
-from random import choice
+import random
 
 # Get a random choice from the list provided
-coin = choice(["heads", "tails"])
-print(coin)
+# coin = choice(["heads", "tails"])
+# print(coin)
+
+# Get a random integer from (a, b) inclusive range
+number = random.randint(1, 10)
+print(number)
