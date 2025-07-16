e5e05ae6a7da13bffb11b38b6c2f8f657d70a938
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-16 09:49:27 +0530

    Slicing in cmd-line args

--- week_4/name.py
+++ week_4/name.py
@@ -4,9 +4,17 @@ import sys
 
 # sys.exit function
 # exiting program prematurely if error exist
+# if len(sys.argv) < 2:
+#     sys.exit("Too few arguements")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguements")
+
+# print("Hello, my name is", sys.argv[1])
+
+# Slicing
+# Multiple Cmd line arguements
 if len(sys.argv) < 2:
     sys.exit("Too few arguements")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguements")
 
-print("Hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+    print("Hi, my name is", arg)
