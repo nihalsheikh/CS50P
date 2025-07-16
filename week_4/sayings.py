86e96d1badfe3e50da7aec5a3fd66d091ae2c7e7
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-16 12:18:41 +0530

    Greet library

---
+++ week_4/sayings.py
@@ -0,0 +1,14 @@
+# Week 4: Libraries
+# Making a library for myself: stored locally
+def main():
+    hello("world")
+    goodbye("world")
+
+def hello(name):
+    print(f"hello, {name}")
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+
+main()
