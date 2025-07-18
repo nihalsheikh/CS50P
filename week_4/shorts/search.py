af0baff3763420942ed960321d51536d1e3b32c1
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-18 12:22:42 +0530

    Packages and Dunder init file

--- week_4/shorts/search.py
+++ week_4/shorts/search.py
@@ -1,11 +1,15 @@
 # Week 4: Shorts: Modules and Packages
 # for searching artworks
 # import artworks
-from artworks import get_artworks
+# from artworks import get_artworks
+# Importing Package: Museum.artworks
+from museum.artworks import get_artworks
 
 # for searching artists
 # import artists
-from artists import get_artists
+# from artists import get_artists
+# Importing Package: Museum.artists
+from museum.artists import get_artists
 
 def main():
     # calling artwork module
