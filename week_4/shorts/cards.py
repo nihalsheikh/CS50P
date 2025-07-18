e5300e8f3e5d8695e94cc6799e9d34bfeae26d4b
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-18 15:36:29 +0530

    random.choice

---
+++ week_4/shorts/cards.py
@@ -0,0 +1,15 @@
+# Week 4: Shorts - random module
+import random
+
+cards = ["jack", "queen", "king"]
+
+def main():
+    # choice: returns a single value from the List passed to it
+    # my_card = random.choice(cards)
+    # print(my_card)
+
+    # choices: returns multiple value when specified
+    my_cards = random.choice(cards)
+    print(my_cards)
+
+main()
