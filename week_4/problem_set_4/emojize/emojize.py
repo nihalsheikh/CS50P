134a8944b27340c3248e2fc37aefb35517f2778c
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-19 09:02:09 +0530

    Week 4: Problem 1 solved

--- week_4/problem_set_4/emojize/emojize.py
+++ week_4/problem_set_4/emojize/emojize.py
@@ -1,2 +1,21 @@
 # Week 4: Problem Set 4
 # Convert input string to emoji
+# Using emoji library
+import emoji
+
+
+def main():
+    # get user input fro emoji
+    text = input("Input: ").strip()
+    print(f"Output: {set_emoji(text)}")
+
+
+# change text to emoji
+def set_emoji(e):
+    try:
+        return emoji.emojize(e, language="alias", variant="emoji_type")
+    except (ValueError, KeyError, AttributeError):
+        return "Unknown Emoji"
+
+
+main()
