1c2fb623ac489f077a9c93249093d941c5c68608
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-21 13:15:32 +0530

    Week 4 Problem 2 submitted

--- week_4/problem_set_4/figlet/figlet.py
+++ week_4/problem_set_4/figlet/figlet.py
@@ -1,6 +1,6 @@
 # Week 4: Problem 2
 # convert user input text to ascii art, if font provided, art in that font, if not then random font
-from pyfiglet import Figlet # import ascii art package called: pyfiglet
+from pyfiglet import Figlet  # import ascii art package called: pyfiglet
 import random
 import sys
 
@@ -18,7 +18,9 @@ def main():
         # choose one random font, when sys.argv length is 0
         font = random.choice(fonts)
         print(text_art(text, font))
-    elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in fonts:
+    elif (
+        len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in fonts
+    ):
         # user input of text
         text = input("Input: ").strip()
         print(text_art(text, sys.argv[2]))
