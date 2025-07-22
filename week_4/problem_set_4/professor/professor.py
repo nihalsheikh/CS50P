8201fd2c26ffde257b7e893f306dd9ff06cee554
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-22 08:52:50 +0530

    Week 4 Problem 5 started

---
+++ week_4/problem_set_4/professor/professor.py
@@ -0,0 +1,6 @@
+# Week 4: Problem 5
+# make a func, such that user inputs the level in range [1,2,3]
+# that level i sused to maek sum arithmetic questions (only addition)
+# a fucn in this return two randm ints x and y, use them for making questions
+# count the times, questions have been answered, if it exceed 3, just return answer
+# also prompt the user again and again if nums are negative ints, or not nums
