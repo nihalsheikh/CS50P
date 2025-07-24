fb305023dac70c318f9a48e0b147005171551b10
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-24 07:47:40 +0530

    Exception handling with Assertion

--- week_5/test_calculator.py
+++ week_5/test_calculator.py
@@ -8,11 +8,31 @@ def main():
 
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared is not 4")
+    # using assert keyword, we may also encounter error aliongside, so use Exception Handling
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("Error: 2 squared was not 4")
 
-    if square(3) != 9:
-        print("3 squared is not 9")
+    try:
+        assert square(- 2) == 4
+    except AssertionError:
+        print("Error: -2 squared was not 4")
+
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("Error: 3 squared was not 9")
+
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("Error: -3 squared was not 9")
+
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("Error: 0 squared was not 0")
 
 
 if __name__ == "__main__":
