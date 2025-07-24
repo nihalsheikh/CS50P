95a7227799e21d3e301f7f9fe3e9e94d5ad6786f
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-24 09:00:44 +0530

    write code with return values instead of side effects

--- week_5/hello.py
+++ week_5/hello.py
@@ -1,11 +1,11 @@
 # Week 5: Unit tests
 def main():
     name = input("Enter your name: ")
-    hello(name)
+    print(hello(name))
 
 
 def hello(to="world"):
-    print("hello,", to)
+    return f"hello, {to}"
 
 
 if __name__ == "__main__":
---
+++ week_5/test_hello.py
@@ -0,0 +1,10 @@
+# Week 5: Unit tests
+from hello import hello
+
+
+def test_default():
+    assert hello() == "hello, world"
+
+
+def test_argument():
+    assert hello("Nihal") == "hello, Nihal"
