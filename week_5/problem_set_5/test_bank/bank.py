3813425322b7671de9cf66ac2cb6f3d462cdafd3
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-25 07:13:31 +0530

    Week 5: Problem 2 submitted

--- week_5/problem_set_5/test_bank/bank.py
+++ week_5/problem_set_5/test_bank/bank.py
@@ -1,11 +1,18 @@
 # Week 5: Problem 2
 # re-make the solution for the program in problem set 1
 def main():
-    ...
+    greet = input("Greetings: ").strip()
+    print(f"${value(greet)}")
 
 
 def value(greeting):
-    ...
+    greet = greeting.lower()
+    if greet.startswith("hello"):
+        return 0
+    elif greet.startswith("h"):
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
--- week_5/problem_set_5/test_bank/test_bank.py
+++ week_5/problem_set_5/test_bank/test_bank.py
@@ -1,1 +1,33 @@
 # Week 5: Problem 2
+#  test cases for bank problem
+import pytest
+from bank import value
+
+
+def test_greet_hello():
+    assert value("hello") == 0
+    assert value("hello, how is your day?") == 0
+    assert value("Hello") == 0
+    assert value("Hello, how you doing?") == 0
+
+
+def test_greet_h():
+    assert value("hi, nice to meet you!") == 20
+    assert value("how is your day?") == 20
+    assert value("hey") == 20
+    assert value("Hola amigo") == 20
+
+
+def test_not_h():
+    assert value("What's happening?") == 100
+    assert value("What's up") == 100
+
+
+def test_case_insensitivty():
+    assert value("HELLo there, how you doin?") == 0
+    assert value("HEy ya'll") == 20
+    assert value("what's up amigo?") == 100
+
+# def test_no_return():
+#     with pytest.raises(AttributeError):
+#         value(3)
