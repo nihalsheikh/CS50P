c242c7fed05f432321c3cbcb278c5a79f6e094cc
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-26 08:22:47 +0530

    Reading the file

--- week_6/names.py
+++ week_6/names.py
@@ -1,7 +1,14 @@
 # Week 6: File I/O
 
-name = input("What's your name? ")
+# Writing into the file
+# name = input("What's your name? ")
 
-# "w": for overwriting previous data. "a": to append to existing data
-with open("names.md", "a") as file:
-    file.write(f"{name}\n")
+# # "w": for overwriting previous data. "a": to append to existing data
+# with open("names.md", "a") as file:
+#     file.write(f"{name}\n")
+
+
+# Reading the file (or Loading the file)
+with open("names.md", "r") as file:
+    for line in file:
+        print("hello,", line.rstrip())
