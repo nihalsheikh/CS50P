5554e1e73ba382aac6fe24a51e07485c66ff263b
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-27 07:27:56 +0530

    FIND_EDGES filter

--- week_6/shorts/image.py
+++ week_6/shorts/image.py
@@ -5,8 +5,9 @@ def main():
         # print(img.size) # size: (width, height)
         # print(img.format)
         img = img.rotate(360)
-        img = img.filter(ImageFilter.BLUR)
-        img.save("blur.jpeg")
+        # img = img.filter(ImageFilter.BLUR)
+        img = img.filter(ImageFilter.FIND_EDGES)
+        img.save("edges.jpeg")
 
 
 main()
