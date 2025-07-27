dae3260a3413256ae9e15cda9179924042ce5bbb
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-27 07:22:31 +0530

    Update 3: timings updated at Sun Jul 27 07:22:31 AM IST 2025

--- week_6/shorts/image.py
+++ week_6/shorts/image.py
@@ -1,10 +1,12 @@
-from PIL import Image
-
+from PIL import Image, ImageFilter
 
 def main():
     with Image.open("in.jpg") as img:
-        print(img.size) # size: (width, height)
-        print(img.format)
+        # print(img.size) # size: (width, height)
+        # print(img.format)
+        img = img.rotate(360)
+        img = img.filter(ImageFilter.BLUR)
+        img.save("blur.jpeg")
 
 
 main()
