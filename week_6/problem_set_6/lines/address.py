3cbb5c5316472ed78149fcff4daddbef6a3227bf
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-28 07:46:18 +0530

    Week 6: Problem 1 started

---
+++ week_6/problem_set_6/lines/address.py
@@ -0,0 +1,12 @@
+# Week 6: File I/O
+import csv
+
+students = []
+
+with open("address.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"], "house": row["house"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} of house {student['house']} lives in {student['home']}")
---
+++ week_6/problem_set_6/lines/lines.py
@@ -0,0 +1,50 @@
+# Week 6: Problem 1
+# Take a user input as a cmd-line arg for file name or path
+# Find the total line of code in a python file, don't count line starting with "#" or " "
+# and exit if file name does not ends with .py or no file found
+import sys
+import os
+import errno
+
+
+def main():
+    # try:
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+    if len(sys.argv) == 2:
+        if not os.path.isfile(sys.argv[1]):
+            raise FileNotFoundError("File does not exist")
+
+        if sys.argv[1][-3:] != ".py":
+            sys.exit("Not a python file")
+
+        total_lines = lines_of_code(sys.argv[1])
+    # except FileNotFoundError:
+    #     sys.exit("File does not exist")
+
+    print(total_lines)
+
+
+def lines_of_code(filename_or_file_path):
+    try:
+        with open(filename_or_file_path, "r") as file:
+            reader = file.readlines()
+            count = 0
+
+            for row in reader:
+                if row.lstrip().startswith("#") or row.lstrip().startswith(" "):
+                    continue
+                else:
+                    count += 1
+            return count
+    except:
+        pass
+    ...
+
+
+if __name__ == "__main__":
+    main()
