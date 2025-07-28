571c55fc7158854277730f17118b547dd3e213e9
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-28 10:00:59 +0530

    Week 3: Problem 3 started

---
+++ week_6/problem_set_6/scourgify/scourgify.py
@@ -0,0 +1,14 @@
+# Week 6: Problem 3
+# Take tow cmd-line args, 1: a csv file as input with name and house as the headers/columns
+# 2: name of the new csv file with first, last and house columns
+import os
+import csv
+
+
+def main():
+    ...
+
+def new_file():
+    ...
+
+main()
