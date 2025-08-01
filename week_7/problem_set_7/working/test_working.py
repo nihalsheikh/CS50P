b77a9d2bb9dc31b767ac4c7249ebba2ceee1ef83
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-01 10:53:35 +0530

    Week 7: Problem 3 solved and submitted

--- week_7/problem_set_7/working/test_working.py
+++ week_7/problem_set_7/working/test_working.py
@@ -1,1 +1,40 @@
 # Week 7: Test case for Problem 3
+import pytest
+from working import convert
+
+
+def test_default():
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5:00 PM") == "09:00 to 17:00"
+
+
+def test_invalid_input():
+    with pytest.raises(ValueError):
+        assert convert("9:00 AM - 5:00 PM")
+        assert convert("9 AM - 5 PM")
+        assert convert("9:00 PM - 17:00 AM")
+
+
+def test_invalid_minutes():
+    with pytest.raises(ValueError):
+        assert convert("9:60 AM to 5:60 PM")
+        assert convert("10:-1 AM to 5:-1 PM")
+        assert convert("9:71 AM to 5:93 PM")
+
+
+def test_invalid_hour():
+    with pytest.raises(ValueError):
+        assert convert("17:40 AM to 5:20 PM")
+        assert convert("9:40 AM to 15:33 PM")
+
+
+def test_invalid_time_min_output():
+    with pytest.raises(AssertionError):
+        assert convert("9:53 AM to 10:11 PM") == "09 to 10"
+
+
+def test_invalid_time_hour_output():
+    with pytest.raises(AssertionError):
+        assert convert("10:53 AM to 7:11 PM") == "00:53 to 00:11"
--- week_7/problem_set_7/working/working.py
+++ week_7/problem_set_7/working/working.py
@@ -12,34 +12,44 @@ def main():
 def convert(s): # tp: time period, s: start, e: end, hr: hour, min: minutes
     pattern = r"^(?P<s_hr>\d{0,2}):?(?P<s_min>\d{0,2}) (?P<s_tp>AM|PM) to (?P<e_hr>\d{0,2}):?(?P<e_min>\d{0,2}) (?P<e_tp>AM|PM)$"
 
-    matches = re.search(pattern, s, re.IGNORECASE)
+    try:
+        matches = re.search(pattern, s, re.IGNORECASE)
 
-    if matches:
-        s_hour = matches.group("s_hr")
-        s_min = matches.group("s_min")
-        start_tp = matches.group("s_tp")
-        e_hour = matches.group("e_hr")
-        e_min = matches.group("e_min")
-        end_tp = matches.group("e_tp")
+        if matches:
+            if matches.group("s_min"):
+                s_min = int(matches.group("s_min"))
+            else:
+                s_min = 0
 
-        if 0 <= int(s_min) < 60 and 0 <= int(e_min) < 60:
-            start_hour = 0
-            end_hour = 0
+            if matches.group("e_min"):
+                e_min = int(matches.group("e_min"))
+            else:
+                e_min = 0
 
-            if start_tp == "AM" and int(s_hour) == 12:
-                start_hour = 0
-            if start_tp == "PM" and int(s_hour) != 12:
-                start_hour += 12
+            s_hour = int(matches.group("s_hr"))
+            start_tp = matches.group("s_tp")
 
-            if end_tp == "AM" and int(s_hour) == 12:
-                end_hour = 0
-            if end_tp == "PM" and int(s_hour) != 12:
-                end_hour += 12
+            e_hour = int(matches.group("e_hr"))
+            end_tp = matches.group("e_tp")
 
-        return matches
-    else:
+            if 1 <= s_hour <= 12 and 1 <= e_hour <= 12 and 0 <= s_min < 60 and 0 <= e_min < 60:
+                if start_tp == "AM" and s_hour == 12:
+                    s_hour = 0
+                elif start_tp == "PM" and s_hour != 12:
+                    s_hour += 12
+
+                if end_tp == "AM" and e_hour == 12:
+                    e_hour = 0
+                elif end_tp == "PM" and e_hour != 12:
+                    e_hour += 12
+
+                return f"{s_hour:02}:{s_min:02} to {e_hour:02}:{e_min:02}"
+            else:
+                raise ValueError("'Hour' or 'Minutes' not in correct format")
+        else:
+            raise ValueError("Incorrect time format")
+    except ValueError:
         raise ValueError
-
 
 if __name__ == "__main__":
     main()
