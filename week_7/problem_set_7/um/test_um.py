96b71b6242d9d8dd5a0cf3ca1a96502cb7cdc9ec
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-01 16:28:17 +0530

    Week 7 Problem 4 solved and submitted

--- week_7/problem_set_7/um/test_um.py
+++ week_7/problem_set_7/um/test_um.py
@@ -1,1 +1,25 @@
 # Week 7: test case for program 4
+from um import count
+import pytest
+
+
+def test_default():
+    assert count("yummy") == 0
+    assert count("hello, um, world") == 1
+
+
+def test_invalid_input():
+    with pytest.raises(TypeError):
+        assert count(1)
+
+def test_not_whole_string():
+      assert count("yummy") == 0
+
+def test_whitespace_with_um():
+      assert count("hello  um  thereum") == 1
+
+def test_um_case_insensitive():
+     assert count("um") == 1
+     assert count("UM") == 1
+     assert count("uM") == 1
+     assert count("Um") == 1
--- week_7/problem_set_7/um/um.py
+++ week_7/problem_set_7/um/um.py
@@ -1,1 +1,23 @@
 # Week 7: Problem 4
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    pattern = r"\b" + re.escape("um") + r"\b"
+    um_count = 0
+
+    matches = re.findall(pattern, s, re.IGNORECASE)
+
+    for string in matches:
+        um_count += 1
+
+    return um_count
+
+
+if __name__ == "__main__":
+    main()
