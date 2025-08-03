c29ab291a47d14bfdd0c4b299078897633b3678a
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-03 20:46:30 +0530

    Week 8 shorts completed

--- week_8/student.py
+++ week_8/student.py
@@ -1,19 +1,26 @@
 # Week 8
-import sys
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name arg")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    print(student)
 
 
 def get_student():
-    try:
-        name = input("Name: ")
-        house = input("House: ")
-        return [name, house]
-    except TypeError:
-        sys.exit("Invalid Input: Name or House")
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
